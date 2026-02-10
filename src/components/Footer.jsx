b1730580735b74ec371fea93c692e65ae5918b1b
Allyssa Dalton <allyssamarie06@icloud.com>
2026-02-10 18:36:25 +0000

    added footer info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,11 @@ const Footer = () => {
     return (
         <footer className="bg-dark-bg py-8 px-6 border-t border-gray-900 text-center md:text-left flex flex-col md:flex-row justify-between items-center text-gray-600 font-mono text-xs">
             <div className="mb-4 md:mb-0">
-                &copy; {new Date().getFullYear()} MARTIN VIZCAINO. ALL RIGHTS RESERVED.
+                &copy; {new Date().getFullYear()} ALLYSSA DALTON. ALL RIGHTS RESERVED.
             </div>
             <div className="flex gap-6">
                 <span>EST. 2026</span>
-                <span>INDY, USA</span>
+                <span>USA</span>
             </div>
         </footer>
     );
